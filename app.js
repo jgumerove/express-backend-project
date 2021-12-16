85da26fef35ab049d44568bdfeb2763a65a5823b
jgumerove <josh.gumerove@gmail.com>
2021-12-16 16:31:33 -0500

    add req query route

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 //below gives us express functionality
 const app = express();
 const users = [{age: 21, email: "jgumerove"}, {age: 23, email: "jgums"}]
+const authors = [{name: "steve"}, {name: "Joe"}, {name: "J.R Tolken"}]
 
 app.listen(3001, () => console.log("listening on port 3001"))
 //below makes a get request -- notice takes in a call back as second param
@@ -32,10 +33,24 @@ app.get("/users/:email", (req, res) => {
     const { email} = req.params
     const user = users.find((user) => user.email === email)
     if(user){
-        res.send(user)
+        //do not need to put the 200 status thing
+        res.status(200).send(user)
     }
     else
     {
         res.send(404)
     }
+})
+
+app.get("/authors", (req, res) => {
+    const { name } = req.query
+    if (name) {
+        const author = authors.find((author) => author.name === name)
+        res.status(200).send(author)
+    }
+
+    else {
+        res.status(404).send("not found")
+    }
+    // const user = users.find((user) => user.email === email)
 })
