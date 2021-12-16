1e6c5aa96d14e9cd08a5c5374148bfef710a96b2
jgumerove <josh.gumerove@gmail.com>
2021-12-16 15:27:23 -0500

    create routes

--- app.js
+++ app.js
@@ -3,8 +3,24 @@ const express = require('express');
 //cannot use import statement
 //below gives us express functionality
 const app = express();
+const users = [{age: 21, email: "jgumerove"}, {age: 23, email: "jgums"}]
 
 app.listen(3001, () => console.log("listening on port 3001"))
+//below makes a get request -- notice takes in a call back as second param
 app.get('/', (req, res) => {
-    res.send(200)
+    res.send({
+        firstName: "Josh",
+        lastName: "Gumerove"
+    })
 });
+//get to another route
+app.get("/users", (req, res) => {
+    res.send(users)
+})
+
+//get to another route
+app.get("/random", (req, res) => {
+    res.send({
+        food: "turkey"
+    })
+})
