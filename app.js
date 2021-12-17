c7a17b0d17af622841707b5cac7ff68d9a89cf41
jgumerove <josh.gumerove@gmail.com>
2021-12-17 15:49:34 -0500

    test auth

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const express = require('express');
 const app = express();
 const users = [{age: 21, email: "jgumerove"}, {age: 23, email: "jgums"}]
 const authors = [{name: "steve"}, {name: "Joe"}, {name: "J.R Tolken"}]
+//to enable us to send requests
 app.use(express.urlencoded({ extended: false}))
+//to enable us to retrieve json -- also a middleware function (detects JSON Payloads)
+app.use(express.json())
 
 app.listen(3001, () => console.log("listening on port 3001"))
 //below makes a get request -- notice takes in a call back as second param
@@ -56,6 +59,13 @@ app.get("/authors", (req, res) => {
     // const user = users.find((user) => user.email === email)
 })
 
-app.post("/", (req, res) => {
-    console.log(req.body)
+app.post("/users", (req, res) => {
+    const { authorization } = req.headers
+    if (authorization && authorization === "3030") {
+        console.log("successful login")
+    }
+    else
+    {
+        console.log("who are you")
+    }
 })
